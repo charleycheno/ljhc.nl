0803f49bd0ecc07948ceed3d451cf387a0728ea9
Charley <charleychen2006@gmail.com>
2024-08-24 15:14:57 +0200

    Add background color to background

--- components/Background.js
+++ components/Background.js
@@ -1,6 +1,6 @@
 export default function Background() {
   return (
-    <div className="flex flex-col items-center p-16 dark:bg-neutral-950">
+    <div className="flex flex-col items-center p-16 bg-neutral-100 dark:bg-neutral-950">
       <h2 className="font-bold text-3xl mb-10">Background</h2>
       <p className="leading-8 max-w-[768px] mx-auto">I began my journey in the digital realm in 2016, initially diving into game development, where I sharpened my skills in programming, design, and problem-solving. This foundation in creating interactive and visually engaging experiences naturally led me to start getting into the world of web development in 2020. Since then, I've been dedicated to crafting responsive, user-friendly websites and web applications, leveraging a variety of technologies such as HTML, CSS, JavaScript, and modern frameworks. My background in game development, combined with my love for languages — both programming and human — has given me a unique perspective on user experience and interactivity. This allows me to build dynamic, functional websites that not only meet technical requirements but also engage and communicate effectively with users from diverse backgrounds.</p>
     </div>
