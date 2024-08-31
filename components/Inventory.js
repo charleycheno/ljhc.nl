b2f2f5f401ec6649cfa699b952ceac887d4fb842
Charley <charleychen2006@gmail.com>
2024-08-31 16:28:29 +0200

    Add schoolnotes.ovh to inventory

--- components/Inventory.js
+++ components/Inventory.js
@@ -5,6 +5,7 @@ export default function Inventory() {
     <div className="flex flex-col items-center p-4 pt-16 sm:p-16 bg-neutral-100 dark:bg-neutral-900">
       <h1 className="font-bold text-3xl mb-10">Inventory</h1>
       <div className="flex flex-col gap-20">
+        <Project image="/assets/schoolnotes.ovh.png" year="2024" url="https://schoolnotes.ovh" title="schoolnotes.nl" source="https://github.com/charleycheno/schoolnotes.ovh" technologies={["Vite", "VitePress", "Vue.js", "Markdown"]} description="Een website waar ik aantekeningen maak van belangrijke examenstof." />
         <Project image="/assets/ljhc.nl.png" year="2024" url="https://ljhc.nl" title="LJHC.nl" source="https://github.com/charleycheno/ljhc.nl" technologies={["Next.js", "Tailwind CSS", "Framer Motion", "NextUI"]} description="This is a Next.js portfolio website for myself!" />
       </div>
     </div>
